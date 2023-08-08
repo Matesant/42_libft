8e96096dc4e45b160f1b26d4355fb39eac1a0d9a
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2023-08-08 17:09:24 -0300

    Libft_42

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 10:36:01 by matesant          #+#    #+#             */
+/*   Updated: 2023/08/08 16:59:52 by matesant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 18:40:34 by matesant          #+#    #+#             */
+/*   Updated: 2023/08/04 16:44:42 by matesant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (i < size && dst[i])
+		i++;
+	while (src[j] && ((i + j + 1) < size))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if ((i + j) < size)
+		dst[i + j] = '\0';
+	return (i + ft_strlen(src));
+}
